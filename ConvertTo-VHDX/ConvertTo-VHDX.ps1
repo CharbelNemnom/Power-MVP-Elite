52ae2690e4f18f6291b202d871fe430029a6b90b
CharbelNemnom <charbel.nemnom@outlook.com>
2018-02-12 16:11:29 +0400

    ConvertTo-VHDX Update

--- ConvertTo-VHDX/ConvertTo-VHDX.ps1
+++ ConvertTo-VHDX/ConvertTo-VHDX.ps1
@@ -36,25 +36,36 @@ param (
     [Parameter(Position=0, Mandatory=$true, HelpMessage = 'Source Path')]
     [Alias('Source')]
     [String]$SourcePath,
+
     [Parameter(Position=1, Mandatory=$true, HelpMessage = 'Destination Path')]
     [Alias('Destination')]
     [String]$DestinationPath
 )
 
+Write-Verbose -Message "Checking the source path..."
 If (-not(Test-Path -Path "$SourcePath\*" -Filter *.VHD)){
     Write-Warning -Message "Source Path does not contain a valid VHD format, Please specify a correct source path"
     Exit
 }
 
+Write-Verbose -Message "Checking the destination path..."
 If (!(Test-Path -Path "$DestinationPath")){
     Write-Warning -Message "Destination Path does not exist, Please specify a correct destination path"
     Exit
 }
 
+Write-Verbose -Message "Conversion starts..."
 # Convert-VHD to VHDX
 Get-ChildItem -Path "$SourcePath" -Recurse -Filter *.VHD | `
 ForEach-Object {Convert-VHD -Path $_.FullName -Destination ("$DestinationPath" + ".vhdx")}
 
+Write-Verbose -Message "Set VHDX to Physical Sector Size 4K..."
 # Set-VHDX to 4K
 Get-ChildItem -Path "$DestinationPath" -Recurse -Filter *.VHDX | `
 ForEach-Object {Set-VHD -Path $_.FullName -PhysicalSectorSizeBytes 4096}
+
+[ValidateSet('Yes','No')]$Answer = Read-Host "`nDo you want to delete the source VHD files? Enter Yes/No"
+If ($Answer -eq 'Yes') {
+Write-Verbose -Message "Deleting the source VHD files..."
+Remove-Item -Path "$SourcePath\*" -Recurse -Force
+}
--- ConvertTo-VHDX/readme.md
+++ ConvertTo-VHDX/readme.md
@@ -2,3 +2,24 @@
 ## VHDX Conversion Tool
 
 This script is written to convert a multiple virtual hard disks (VHD) to VHDX format.
+
+Run the script as follows:
+
+.\ConvertTo-VHDX.ps1 -SourcePath D:\VHDFolder\ -DestinationPath D:\ -Verbose
+
+This example converts all VHD in a source path to VHDX in a destination path, and finally set the PhysicalSectorSizeBytes to 4096.
+A converted VHDX file has a physical sector size of 512 Bytes. However, when you create a new VHDX file it has a physical sector size of 4K. 
+
+Here is a screenshot showing the conversion is completed.
+
+---- Tested environment -----
+- Windows Server Hyper-V Version 1709
+- EN-US language OS
+
+---- Tested environment -----
+- Windows Server Hyper-V Version 1607 
+- EN-US language OS
+
+---- Tested environment -----
+- Windows Server 2012 R2
+- EN-US language OS
